794f6d0f8922aa8378f8ac1a0bdf5185e1b62772
Stephen Mudie <stephenm@ansto.gov.au>
2025-09-10 13:45:02 +1000

    Dynamic basepath for documentation.

--- src/docs-main.tsx
+++ src/docs-main.tsx
@@ -1,7 +1,12 @@
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './docs/routeTree.gen'
 
-const router = createRouter({ routeTree })
+
+const basepath =
+  import.meta.env.MODE === 'github-pages' ? '/sci-comp-ui' : ''
+
+
+const router = createRouter({ routeTree, basepath })
 
 declare module '@tanstack/react-router' {
   interface Register {
