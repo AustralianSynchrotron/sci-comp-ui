8bdb207847e5efca3f6d8a64ae3062bd40fddd65
Michelle Chan <chanm@ansto.gov.au>
2026-04-20 14:53:55 +1000

    Updated h264 demo component intro to mention ctrl+click feature

--- src/docs/routes/experimental/camera-control-h264.tsx
+++ src/docs/routes/experimental/camera-control-h264.tsx
@@ -67,9 +67,9 @@ function CameraControlWebsocketH264Page() {
                             <CardTitle>Position Control</CardTitle>
                             <CardDescription>
                                 <p>
-                                    Video display for h264 over websocket streams. Source is the AS Websocket Stream 
-                                    (private repo at this time). The h264 stream is provided to the camera-control component
-                                    via a h264 provider.
+                                    Video display for h264 over websocket streams. Source is the AS Websocket Stream
+                                    (private repo at this time). The h264 stream is provided to the camera-control
+                                    component via a h264 provider.
                                 </p>
                                 <p>Click and drag to zoom in.</p>
                                 <p>Hold space and drag to move (will not move when at the edge of the image).</p>
